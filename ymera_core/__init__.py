bb38406353645e145121c5b7b8d3f939c5539975
mmansour88 <mfmansour1@outlook.com>
2025-08-01 20:37:56 +0400

    Initialize ymera_core module with version and metadata

---
+++ ymera_core/__init__.py
@@ -0,0 +1,10 @@
+"""
+YMERA Core - Enterprise Backend Infrastructure
+-------------------------------------------
+This module contains the core backend infrastructure for the YMERA platform.
+Version: 1.0.0
+"""
+
+__version__ = "1.0.0"
+__author__ = "YMERA Enterprise"
+__license__ = "Proprietary"
